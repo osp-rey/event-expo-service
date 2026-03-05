5425f20e4d3c20316a136040e0f82cb6e2d595b3
pauchok-sk <pauchoooooook@gmail.com>
2026-03-05 19:44:52 +0300

    s-location

--- src/js/files/interactiveMap.js
+++ src/js/files/interactiveMap.js
@@ -3,23 +3,81 @@ export default function interactiveMap() {
 
   if (map) {
     const regions = map.querySelectorAll(".loc-btn");
+    const popup = document.querySelector("#interactive-map-popup");
+    const titlePopup = document.querySelector("#interactive-map-title");
+    const listPopup = document.querySelector("#interactive-map-list");
+    const btnClose = document.querySelector("#interactive-map-close");
 
-    regions.forEach(region => {
-      console.log(region)
+    btnClose.addEventListener("click", () => {
+      popup.classList.remove("_active");
+
+      setTimeout(() => {
+        handlerClear();
+      }, 300);
+    });
+
+    regions.forEach((region) => {
       region.addEventListener("mouseover", () => {
         const currentName = map.querySelector(region.getAttribute("href"));
-
         if (currentName) {
           currentName.classList.add("_hover");
         }
-      })
+      });
+      region.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const dataItems = JSON.parse(region.dataset.items);
+        const dataTitle = region.dataset.name;
+
+        regions.forEach((r) => r.classList.remove("_active"));
+        region.classList.add("_active");
+
+        handlerOpen(dataTitle, dataItems);
+      });
+
       region.addEventListener("mouseout", () => {
         const currentName = map.querySelector(region.getAttribute("href"));
 
         if (currentName) {
           currentName.classList.remove("_hover");
         }
-      })
-    })
+      });
+    });
+
+    function handlerOpen(title, items) {
+      let delay = 0;
+
+      if (popup.classList.contains("_active")) {
+        handlerClear();
+        delay = 300;
+      }
+
+      setTimeout(() => {
+        titlePopup.textContent = title;
+        items.forEach((item) => {
+          const li = document.createElement("li");
+
+          li.innerHTML = `<span class="num text-linear">${item[0]}</span><span class="val">${item[1]}</span>`;
+          listPopup.appendChild(li);
+        });
+        popup.classList.add("_active");
+      }, delay);
+    }
+
+    function handlerClear() {
+      titlePopup.classList.add("_hide");
+      listPopup.classList.add("_hide");
+
+      setTimeout(() => {
+        listPopup.innerHTML = "";
+        titlePopup.textContent = "";
+      }, 100);
+
+      setTimeout(() => {
+        titlePopup.classList.remove("_hide");
+        listPopup.classList.remove("_hide");
+      }, 400);
+    }
   }
 }
